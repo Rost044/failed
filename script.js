4c8a1936a8d92af0a72f62227970edb48ea0aafd
Rost044 <randrusko044@gmail.com>
2018-08-11 13:39:52 +0400

    head

---
+++ script.js
@@ -0,0 +1,63 @@
+console.log(areArraysEqual([1,2,3], [1,2]));
+console.log(areArraysEqual([1,2,3], [1,4,3]));
+console.log(areArraysEqual([1,2,3], ["1",2,3]));
+console.log(areArraysEqual([1,2,3], [1,2,3]));
+
+
+function areArraysEqual(A,B) {
+         for(var i = 0; i<A.lenght; i++){
+         if(!(A[i] === B[i])){
+               returne
+    
+             }
+
+                        }
+
+        areArraysEqual([1,2,3], [1,2])
+        areArraysEqual([1,2,3], [1,4,3])
+        areArraysEqual([1,2,3], ["1",2,3])
+        areArraysEqual([1,2,3], [1,2,3])
+
+
+
+}
+// var canvas = document.getElementById("sandbox"),
+//     context = canvas.getContext("2d"),
+//     square, circle,
+//     line = new Path2D();
+
+
+
+// context.fillRect(0, 0, 200, 250);
+
+
+/////////////////////
+
+// square = new Path2D();
+// square.moveTo(50, 50);
+// square.lineTo(250, 50);   
+// square.lineTo(250, 250);
+// square.lineTo(50, 250);
+// square.lineTo(50, 50);
+
+// context.stroke(square);
+
+// circle = new Path2D();
+// circle.arc(150, 150, 100, 0, 2 * Math.PI);
+
+// context.fill(circle);
+
+
+//////////////////////////////
+
+
+// line.moveTo(0, 0);
+// line.lineTo(300, 300);   
+
+// context.lineWidth = 5;
+// context.stroke(line);
+
+// context.fillStyle = "silver";
+// context.fillRect(0,0,200,200);
+
+// context.fillStyle = "rgba(0,255,0,0.5)";
